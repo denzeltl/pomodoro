a60a459a45f52b9b707006242c884e9b1e0b2ed6
Denzel <denzeltlee@gmail.com>
2020-08-06 23:20:37 +0800

    Implement focus to break and vice versa function

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,13 +4,13 @@ import momentDurationFormatSetup from 'moment-duration-format';
 import { TimerContext } from '../context/TimerContext';
 
 const Display = () => {
-    const { sessionTime } = useContext(TimerContext);
+    const { sessionTime, sessionName } = useContext(TimerContext);
     momentDurationFormatSetup(moment);
     const formattedTime = moment.duration(sessionTime, 's').format('h:m:ss');
 
     return (
         <div className="display">
-            <p className="label">POMODORO</p>
+            <p className="label">{sessionName}</p>
             <p className="timer">{formattedTime}</p>
         </div>
     );
--- src/context/TimerContext.js
+++ src/context/TimerContext.js
@@ -1,11 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const TimerContext = createContext();
 
 const TimerContextProvider = ({ children }) => {
-    const [sessionTime, setSessionTime] = useState(10);
+    const [focusLength, setFocusLength] = useState(5);
+    const [breakLength, setBreakLength] = useState(3);
+    const [sessionName, setSessionName] = useState('FOCUS');
+    const [sessionTime, setSessionTime] = useState(focusLength);
     const [currentlyRunning, setCurrentlyRunning] = useState(false);
     const [intervalId, setIntervalId] = useState(null);
+    const [finishedTimer, setFinishedTimer] = useState(false);
+
+    useEffect(() => {
+        if (finishedTimer) {
+            if (sessionName === 'FOCUS') {
+                setSessionName('BREAK');
+                setSessionTime(breakLength);
+            } else if (sessionName === 'BREAK') {
+                setSessionName('FOCUS');
+                setSessionTime(focusLength);
+            }
+        }
+        setFinishedTimer(false);
+    }, [finishedTimer]);
 
     const startTimer = () => {
         setCurrentlyRunning(!currentlyRunning);
@@ -15,17 +32,19 @@ const TimerContextProvider = ({ children }) => {
             setIntervalId(null);
         } else {
             const countdown = setInterval(() => {
-                if (sessionTime >= 0) {
-                    setSessionTime((prevTime) => {
+                setSessionTime((prevTime) => {
+                    const newPrevTime = prevTime - 1;
+                    if (newPrevTime >= 0) {
                         return prevTime - 1;
-                    });
-                }
+                    }
+                    setFinishedTimer(true);
+                });
             }, 1000);
             setIntervalId(countdown);
         }
     };
 
-    return <TimerContext.Provider value={{ sessionTime, startTimer, currentlyRunning }}>{children}</TimerContext.Provider>;
+    return <TimerContext.Provider value={{ sessionTime, startTimer, currentlyRunning, sessionName }}>{children}</TimerContext.Provider>;
 };
 
 export default TimerContextProvider;
